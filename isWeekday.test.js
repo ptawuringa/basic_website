93544f1640405c5fe527dd35ac308ed255103904
ptawuringa <60348606+ptawuringa@users.noreply.github.com>
2020-05-13 11:49:47 +0200

    Add files via upload
    My second commit.

---
+++ isWeekday.test.js
@@ -0,0 +1,31 @@
+
+
+
+// fix the `it `functions below to call the isWeekday function correctly to pass the tests.
+
+
+describe("The isWeekday function ", function(){
+  
+    it ("should say that Monday is a weekday", function() {
+        // todo fix this assert to call isWeekday correctly.
+        assert.equal(false, false);
+    });
+  
+    it ("should say that Friday is a weekday", function() {
+        // todo fix this assert to call isWeekday correctly.
+        assert.equal(false, false);
+    });
+    
+    it ("should say that Sunday is not weekday", function() {
+        // todo fix this assert to call isWeekday correctly.
+        assert.equal(true , true) ;
+    });
+  
+    it ("should say that Saturday is not weekday", function() {
+        // todo fix this assert to call isWeekday correctly.
+        assert.equal(true , true);
+    });
+    
+  
+    
+});
